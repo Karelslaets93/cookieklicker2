bd9a0a1e53b51fc47ccbc3ae08f24406f6369919
Karelslaets93 <145328954+Karelslaets93@users.noreply.github.com>
2023-11-20 16:07:28 +0100

    COOKIECLICKER-04Klik effect

--- cookieklicker2/MainWindow.xaml.cs
+++ cookieklicker2/MainWindow.xaml.cs
@@ -22,23 +22,31 @@ namespace cookieklicker2
     public partial class MainWindow : Window
     {
         private int _cookieCount = 0;
+        private double _originalWidth;
+        private double _originalHeight;
 
         public MainWindow()
         {
             InitializeComponent();
+            _originalWidth = imgCookie.Width;
+            _originalHeight = imgCookie.Height;
         }
 
         private void imgCookie_MouseDown(object sender, MouseButtonEventArgs e)
         {
-           
+            _cookieCount++;
+            txtCookieCount.Text = _cookieCount + " Cookies";
+            this.Title = _cookieCount + " Cookies";
+            imgCookie.Width = _originalWidth * 0.9;
+            imgCookie.Height = _originalHeight * 0.9;
         }
 
         private void imgCookie_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            _cookieCount++;
-            txtCookieCount.Text = _cookieCount + " Cookies";
-            this.Title = _cookieCount + " Cookies";
+            imgCookie.Width = _originalWidth;
+            imgCookie.Height = _originalHeight;
         }
     }
 }
+
 

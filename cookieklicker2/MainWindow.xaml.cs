ab81a3ef256a277ae55944b340f5817dbb64e030
Karelslaets93 <145328954+Karelslaets93@users.noreply.github.com>
2023-12-04 19:45:25 +0100

    COOKIECLICKER-15 COOKIECLICKER-16 COOKIECLICKER-17

--- cookieklicker2/MainWindow.xaml.cs
+++ cookieklicker2/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,11 +25,10 @@ namespace cookieklicker2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double _cookieCount = 10000;
+        private double _cookieCount =100000000000000000;
         private double _originalWidth;
         private double _originalHeight;
         private bool _isMouseDown = false;
-
         private int _cursorCount = 0;
         private int _grandmaCount = 0;
         private int _farmCount = 0;
@@ -46,7 +47,18 @@ namespace cookieklicker2
             _timer.Interval = TimeSpan.FromMilliseconds(10);
             _timer.Tick += Timer_Tick;
             _timer.Start();
+
+
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 100;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(2));
+            doubleAnimation.AutoReverse = true;
+            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            progressBar.BeginAnimation(ProgressBar.ValueProperty, doubleAnimation);
         }
+
 
         private void Timer_Tick(object sender, EventArgs e)
         {
@@ -55,16 +67,13 @@ namespace cookieklicker2
             opbrengstPerTick += _grandmaCount * 0.01;
             opbrengstPerTick += _farmCount * 0.08;
             opbrengstPerTick += _mineCount * 0.47;
-            opbrengstPerTick += _factoryCount * 2.60;
+            opbrengstPerTick += _factoryCount * 2.6;
 
             _cookieCount += opbrengstPerTick;
-            int cookieCountAsInt = (int)Math.Floor(_cookieCount);
-            txtCookieCount.Text = cookieCountAsInt + " Cookies";
-            this.Title = cookieCountAsInt + " Cookies";
+            UpdateCookieCount();
 
             UpdateButtons();
         }
-
 
         private void imgCookie_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -78,101 +87,100 @@ namespace cookieklicker2
             if (_isMouseDown)
             {
                 _cookieCount++;
-                txtCookieCount.Text = _cookieCount + " Cookies";
-                this.Title = _cookieCount + " Cookies";
+                UpdateCookieCount();
             }
             _isMouseDown = false;
             imgCookie.Width = _originalWidth;
             imgCookie.Height = _originalHeight;
 
             UpdateButtons();
+
         }
 
         private void btnCursor_Click(object sender, RoutedEventArgs e)
         {
-            int basisprijs = 15;  
+            int basisprijs = 15;
             int nieuwePrijs = (int)Math.Ceiling(basisprijs * Math.Pow(1.15, _cursorCount));
 
             if (_cookieCount >= nieuwePrijs)
             {
                 _cookieCount -= nieuwePrijs;
                 _cursorCount++;
-                txtCookieCount.Text = _cookieCount + " Cookies";
-                this.Title = _cookieCount + " Cookies";
+                UpdateCookieCount();
                 txtCursorCount.Text = "Count: " + _cursorCount;
 
                 UpdateButtons();
+                UpdateCookiesPerSecond();
             }
         }
 
         private void btnGrandma_Click(object sender, RoutedEventArgs e)
         {
-            int basisprijs = 100;  
+            int basisprijs = 100;
             int nieuwePrijs = (int)Math.Ceiling(basisprijs * Math.Pow(1.15, _grandmaCount));
 
             if (_cookieCount >= nieuwePrijs)
             {
                 _cookieCount -= nieuwePrijs;
                 _grandmaCount++;
-                txtCookieCount.Text = _cookieCount + " Cookies";
-                this.Title = _cookieCount + " Cookies";
+                UpdateCookieCount();
                 txtGrandmaCount.Text = "Count: " + _grandmaCount;
 
                 UpdateButtons();
+                UpdateCookiesPerSecond();
             }
         }
 
         private void btnFarm_Click(object sender, RoutedEventArgs e)
         {
-            int basisprijs = 1100;  
+            int basisprijs = 1100;
             int nieuwePrijs = (int)Math.Ceiling(basisprijs * Math.Pow(1.15, _farmCount));
 
             if (_cookieCount >= nieuwePrijs)
             {
                 _cookieCount -= nieuwePrijs;
                 _farmCount++;
-                txtCookieCount.Text = _cookieCount + " Cookies";
-                this.Title = _cookieCount + " Cookies";
+                UpdateCookieCount();
                 txtFarmCount.Text = "Count: " + _farmCount;
 
                 UpdateButtons();
+                UpdateCookiesPerSecond();
             }
         }
 
         private void btnMine_Click(object sender, RoutedEventArgs e)
         {
-            int basisprijs = 12000;  
+            int basisprijs = 12000;
             int nieuwePrijs = (int)Math.Ceiling(basisprijs * Math.Pow(1.15, _mineCount));
 
             if (_cookieCount >= nieuwePrijs)
             {
                 _cookieCount -= nieuwePrijs;
                 _mineCount++;
-                txtCookieCount.Text = _cookieCount + " Cookies";
-                this.Title = _cookieCount + " Cookies";
+                UpdateCookieCount();
                 txtMineCount.Text = "Count: " + _mineCount;
 
                 UpdateButtons();
+                UpdateCookiesPerSecond();
             }
         }
 
         private void btnFactory_Click(object sender, RoutedEventArgs e)
         {
-            int basisprijs = 130000;  
+            int basisprijs = 130000;
             int nieuwePrijs = (int)Math.Ceiling(basisprijs * Math.Pow(1.15, _factoryCount));
 
             if (_cookieCount >= nieuwePrijs)
             {
                 _cookieCount -= nieuwePrijs;
                 _factoryCount++;
-                txtCookieCount.Text = _cookieCount + " Cookies";
-                this.Title = _cookieCount + " Cookies";
+                UpdateCookieCount();
                 txtFactoryCount.Text = "Count: " + _factoryCount;
 
                 UpdateButtons();
+                UpdateCookiesPerSecond();
             }
         }
-
 
         private void UpdateButtons()
         {
@@ -180,7 +188,7 @@ namespace cookieklicker2
             int grandmaPrijs = (int)Math.Ceiling(100 * Math.Pow(1.15, _grandmaCount));
             int farmPrijs = (int)Math.Ceiling(1100 * Math.Pow(1.15, _farmCount));
             int minePrijs = (int)Math.Ceiling(12000 * Math.Pow(1.15, _mineCount));
-            int factoryPrijs = (int)Math.Ceiling(130000 * Math.Pow(1.15, _factoryCount));
+            long factoryPrijs = (long)Math.Ceiling(130000 * Math.Pow(1.15, _factoryCount));
 
             btnCursor.IsEnabled = _cookieCount >= cursorPrijs;
             btnGrandma.IsEnabled = _cookieCount >= grandmaPrijs;
@@ -188,11 +196,45 @@ namespace cookieklicker2
             btnMine.IsEnabled = _cookieCount >= minePrijs;
             btnFactory.IsEnabled = _cookieCount >= factoryPrijs;
 
-            txtCursorCost.Text = "Cursor: " + cursorPrijs;
-            txtGrandmaCost.Text = "Grandma: " + grandmaPrijs;
-            txtFarmCost.Text = "Farm: " + farmPrijs;
-            txtMineCost.Text = "Mine: " + minePrijs;
-            txtFactoryCost.Text = "Factory: " + factoryPrijs;
+            txtCursorCost.Text = "Cursor: " + FormatNumber(cursorPrijs);
+            txtGrandmaCost.Text = "Grandma: " + FormatNumber(grandmaPrijs);
+            txtFarmCost.Text = "Farm: " + FormatNumber(farmPrijs);
+            txtMineCost.Text = "Mine: " + FormatNumber(minePrijs);
+            txtFactoryCost.Text = "Factory: " + FormatNumber(factoryPrijs);
+        }
+
+        private string FormatNumber(double num)
+        {
+            if (num >= 1e12)
+            {
+                return Math.Round(num / 1e12, 3).ToString("0.###") + " Triljoen";
+            }
+            else if (num >= 1e9)
+            {
+                return Math.Round(num / 1e9, 3).ToString("0.###") + " Biljard";
+            }
+            else if (num >= 1e6)
+            {
+                return Math.Round(num / 1e6, 3).ToString("0.###") + " Miljoen";
+            }
+            else
+            {
+                return Math.Round(num).ToString();
+            }
+        }
+
+        private void UpdateCookieCount()
+        {
+            string formattedNumber = FormatNumber(_cookieCount);
+            txtCookieCount.Text = formattedNumber;
+            this.Title = formattedNumber + " Cookies";
+        }
+
+
+        private void UpdateCookiesPerSecond()
+        {
+            double cookiesPerSecond = _cursorCount * 0.1 + _grandmaCount * 1 + _farmCount * 8 + _mineCount * 47 + _factoryCount * 260;
+            txtCookiesPerSecond.Text = cookiesPerSecond + " cookies/second";
         }
 
     }
